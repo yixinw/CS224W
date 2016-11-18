93680c2ab5f716324feb73f8b0a6f46baec68982
yixinw <bessywang1993@gmail.com>
2016-11-17 16:26:01 -0800

    tf-idf feature extraction

---
+++ extract_feature.py
@@ -0,0 +1,37 @@
+'''
+This file reads in the community file, finds all abstract associated with this community, and extracts tf-idf feature for each community.
+'''
+
+from extract_feature_util import *
+import IPython
+import numpy as np
+
+data_dir = '../data/'
+community_filename = '2_cmtyvv.txt'
+abstract_filename = 'abstract_2'
+id_to_abstract_line_filename = 'id_to_abstract_line_2'
+
+# Extract tf-idf features.
+features, dictionary, vocabulary = extract_tfidf(data_dir=data_dir,
+        community_filename=community_filename,
+        abstract_filename=abstract_filename,
+        id_to_abstract_line_filename=id_to_abstract_line_filename)
+
+# Order tf-idf features by weights.
+# features is 100 x 44305 sparse matrix.
+num_top_words = 200
+num_top_community = 10
+num_community, vocabulary_size = features.shape
+community_top_words = []
+
+for community_id in range(num_top_community):
+    community_words_weight = features[community_id]
+    top_word_index = np.argsort(community_words_weight.toarray()[0])[-num_top_words:]
+    community_top_words.append([dictionary[i] for i in top_word_index])
+
+
+IPython.embed()
+
+np.save("top.npy", community_top_words)
+
+
---
+++ extract_feature_util.py
@@ -0,0 +1,48 @@
+'''
+This file implements tf-idf feature extraction.
+If we have time, we need to revise the interface so that
+it does not take in filenames.
+'''
+
+from sklearn.feature_extraction.text import *
+import linecache
+
+def extract_tfidf(data_dir,
+        community_filename,
+        abstract_filename,
+        id_to_abstract_line_filename):
+
+    id_to_abstract_line_map = {}
+    with open(data_dir + id_to_abstract_line_filename, 'r') as id_to_abstract_line_file:
+        for id_to_abstract_line in id_to_abstract_line_file:
+            pair = id_to_abstract_line.strip("\n").split(",")
+            id = int(pair[0])
+            abstract_line = int(pair[1])
+            id_to_abstract_line_map[id] = abstract_line
+
+    with open(data_dir + community_filename, 'r') as community_file:
+        # Extract text content.
+        # A list that stores the concatenated abstract of
+        # each community. Community ID correspond to list index.
+        all_community_abstract = []
+        for community_line in community_file:
+            community_members = \
+                    [int(x) for x in community_line.strip("\n").strip("\t").split("\t")]
+            abstract_list = []
+            for member_id in community_members:
+                if member_id in id_to_abstract_line_map:
+                    abstract_line_number = id_to_abstract_line_map[member_id]
+                    abstract = linecache.getline(
+                            data_dir+abstract_filename,
+                            abstract_line_number)
+                    abstract_list.append(abstract.strip("\n"))
+            all_community_abstract.append(' '.join(abstract_list))
+
+        # Count words and extract tf-idf features.
+        vectorizer = TfidfVectorizer(min_df=1, stop_words='english')
+        word_count = vectorizer.fit_transform(all_community_abstract)
+        dictionary = [x for x in vectorizer.get_feature_names()]
+        vocabulary = vectorizer.vocabulary_
+        return word_count, dictionary, vocabulary
+
+
